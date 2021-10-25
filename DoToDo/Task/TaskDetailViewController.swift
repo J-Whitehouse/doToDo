e8f7ec9d299688c5d148a155cd45c087f67d3073
Jarret <jarrwhit@iu.edu>
2021-10-25 15:41:25 -0400

    Added detail view controller, not linked up, still not sure if v4 works

---
+++ DoToDo/Task/TaskDetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TaskDataViewController.swift
+//  DoToDo
+//
+//  Created by Whitehouse, Jarret M on 10/25/21.
+//
+
+import UIKit
+import Firebase
+
+class TaskDetailViewController: UIViewController {
+    
+    //MARK: OUTLETS
+    
+    @IBOutlet var data: UILabel!
+    @IBOutlet var dueDate: UILabel!
+    
+    
+}
